cec3cc18afec8c4d3215ead01afe6c639a6a0ee1
Trịnh Quốc Hoàng <quochoangvp@gmail.com>
2016-07-19 00:37:55 +0700

    Add jasny-bootstrap package

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,7 @@ elixir(function(mix) {
     mix.sass('login.scss', 'public/templates/assets/css/login.css');
     mix.sass('dashboard.scss', 'public/templates/assets/css/dashboard.css');
     mix.sass('user.scss', 'public/templates/assets/css/user.css');
+    mix.sass('profile.scss', 'public/templates/assets/css/profile.css');
     mix
         .copy('node_modules/font-awesome/fonts', 'public/templates/assets/fonts')
         .copy('node_modules/simple-line-icons/fonts', 'public/templates/assets/fonts')
@@ -32,6 +33,7 @@ elixir(function(mix) {
         .copy('node_modules/bootstrap-datepicker/dist/css/bootstrap-datepicker3.css', 'public/templates/assets/css')
         .copy('node_modules/bootstrap-datepicker/dist/locales/bootstrap-datepicker.vi.min.js', 'resources/assets/js/vendor')
         .copy('node_modules/bootstrap-datepicker/dist/js/bootstrap-datepicker.js', 'resources/assets/js/vendor')
+        .copy('node_modules/jasny-bootstrap/js/fileinput.js', 'resources/assets/js/vendor')
     ;
     mix.scripts([
         'vendor/jquery.js',
@@ -57,6 +59,16 @@ elixir(function(mix) {
         'layout.js',
         'user.js'
     ], 'public/templates/assets/js/user.js');
+
+    mix.scripts([
+        'vendor/jquery.js',
+        'vendor/bootstrap.js',
+        'vendor/jquery.slimscroll.js',
+        'vendor/fileinput.js',
+        'common.js',
+        'layout.js',
+        'profile.js'
+    ], 'public/templates/assets/js/profile.js');
     mix.jade({
         blade: false,
         html: true,
